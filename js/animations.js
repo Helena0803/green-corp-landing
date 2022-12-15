fef3cfa47848ad84bad4849184bab8d1f2760a81
Helena0803 <klena81@list.ru>
2022-12-15 11:39:03 +0300

    Финал

--- js/animations.js
+++ js/animations.js
@@ -1,26 +1,3 @@
-// const INCREASE_NUMBER_ANIMATION_SPEED = 50;
-// function increaseNumberAnimationStep(i, element, endNumber) {
-//     if (i <= endNumber) {
-//         if (i === endNumber) {
-//             element.innerText = i + '+';
-//           } else {
-//             element.innerText = i;
-//           }
-      
-//           i++;
-        
-//         setTimeout(
-//           () => increaseNumberAnimationStep(i, element, endNumber),
-//           INCREASE_NUMBER_ANIMATION_SPEED);
-//     }
-//   }
-
-// function initIncreaseNumberAnimation() {
-//      let element_my = document.querySelector(".features__clients-count");
-//   increaseNumberAnimationStep(0, element_my, 5000);
-// }
-// initIncreaseNumberAnimation();
-
 const INCREASE_NUMBER_ANIMATION_SPEED = 50;
 
 function increaseNumberAnimationStep (i, element, endNumber) {
@@ -44,4 +21,60 @@ function initIncreaseNumberAnimation() {
   let element_my = document.querySelector(".features__clients-count")  
   increaseNumberAnimationStep (0, element_my, 5000) 
 }
-initIncreaseNumberAnimation();
+//initIncreaseNumberAnimation();
+
+document.querySelector('#budjet').addEventListener('change', function handleSelectChange(event) {
+  if (event.target.value === 'other') {
+    const formContainer = document.createElement('div');
+    formContainer.classList.add('form__group');
+    formContainer.classList.add('form__other-input'); // Задание 1
+ 
+    const input = document.createElement('input');
+    input.placeholder = "Введите ваш вариант";
+    input.type = "text"; // Задание 2
+      
+    formContainer.appendChild(input);
+    document.querySelector('#form form').insertBefore(formContainer, document.querySelector('.form__submit')); // Задание 3
+  }
+ 
+  const otherInput = document.querySelector('.form__other-input');
+
+  if (event.target.value !== 'other' && otherInput) { // Задание 5
+  document.querySelector('#form form').removeChild(otherInput); // Задание 4
+  }
+});
+
+function updateScroll() {
+  if (window.scrollY > 0) {
+   let  scroll = document.querySelector("header");
+   scroll.classList.add("header__scrolled");
+  } else {
+    let  scrollRemove = document.querySelector("header");
+    scrollRemove.classList.remove("header__scrolled");
+  }
+  let windowBottomPosition = window.scrollY + window.innerHeight;
+  let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+    if(windowBottomPosition  >= countElementPosition && !animationInited) {
+       animationInited = true;
+      initIncreaseNumberAnimation();
+    }
+  
+}
+window.addEventListener('scroll', updateScroll);
+
+function addSmoothScroll(anchor) {
+  anchor.addEventListener('click', onLinkClick);
+}
+
+function onLinkClick(event) {
+  event.preventDefault();
+  document.querySelector(event.target.getAttribute('href')).scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  addSmoothScroll(anchor);
+});
+
+addSmoothScroll(document.querySelector('.more-button'));
